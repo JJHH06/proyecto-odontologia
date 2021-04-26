7da79da891a73cda835c048caceb90211d9350a9
Jose Hurtarte <JJHH121106@gmail.com>
2021-04-26 13:58:32 -0600

    dientes and condiciones created

--- Backend/database_def.sql
+++ Backend/database_def.sql
@@ -1,1 +1,10 @@
 Create database proyecto_odontologia;
+
+Create table if not exists condiciones_medicas(
+    id_condicion serial primary key,
+    nombre_condicion varchar(30));
+
+Create table if not exists dientes(
+    no_pieza varchar(10) primary key,--formato universal
+    no_pieza_fda varchar(10),
+    nombre_pieza varchar(30));
