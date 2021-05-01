fef5bc1856116d57b4c2ff2c201d7b863de0b94b
Jose Hurtarte <JJHH121106@gmail.com>
2021-05-01 17:54:00 -0600

    Modificación base de datos
    por José Hurtarte y Hansel López

--- Backend/database_def.sql
+++ Backend/database_def.sql
@@ -1,14 +1,20 @@
+--creación de la base de datos
 Create database proyecto_odontologia;
-
+-----------------------------------------
+--Banco de datos de las condiciones preexistentes
 Create table if not exists condiciones_medicas(
     id_condicion serial primary key,
     nombre_condicion varchar(30));
 
-Create table if not exists dientes(
+
+-- contiene un diccionario de cada diente existente
+-- también se puede colocar la boca completa
+
+Create table if not exists piezas_dentales(
     no_pieza varchar(10) primary key,--formato universal
-    no_pieza_fda varchar(10),
     nombre_pieza varchar(30));
 
+--tabla que contiene los datos de cada paciente
 Create table if not exists paciente(
     id_paciente serial primary key,
     nombre text,
@@ -23,40 +29,67 @@ Create table if not exists paciente(
     motivo_consulta text,
     medico text,
     telefono_medico varchar(30),
-    contacto_mergencia text,
+    contacto_emergencia text,
     telefono_emergencia varchar(30)
 );
-
-Create table if not exists diagnostico_dental(
-    id_diagnostico serial primary key,
-    nombre_diagnostico varchar(30)
+--contiene que diagnostico preeexistente tiene cada paciente
+create table if not exists condiciones_paciente(
+    id_paciente int,
+    id_condicion int,
+    primary key (id_paciente, id_condicion),
+    foreign key (id_paciente) references paciente(id_paciente),
+    foreign key (id_condicion) references condiciones_medicas(id_condicion)
 );
 
+--condiciones dentales que se encuentren la cita
+Create table if not exists diagnostico_dental(
+    id_diagnostico serial primary key,
+    nombre_diagnostico varchar(30));
+
+
+--diagnostico dental de cada paciente
 Create table if not exists diagnostico_paciente(
     id_paciente int,
     id_diagnostico int,
-    id_pieza int,
-    primary key (id_paciente,id_diagnostico,id_pieza);
+    no_pieza varchar(10),
+    primary key (id_paciente,id_diagnostico,no_pieza),
+    foreign key (id_paciente) references paciente(id_paciente),
+    foreign key (id_diagnostico) references diagnostico_dental(id_diagnostico),
+    foreign key (no_pieza) references piezas_dentales(no_pieza)
+    );
+
+--cualquier empleado dentro de la clínica
+create table if not exists empleado(
+    id_empleado varchar(20) primary key,
+    password varchar(40),
+    nombre text
+
+);
 
 
+--doctor de la clínica
 Create table if not exists doctor(
-    nombre_usuario varchar(30) primary key,
-    nombre text,
-    contrasena varchar(30)
+    id_empleado varchar(30) primary key,
+    email varchar(60),
+    foreign key (id_empleado) references empleado(id_empleado)
 );
 
+
+--asistente dental de la clínica
 Create table if not exists asistente(
-    nombre_usuario varchar(30) primary key,
-    nombre text,
-    contrasena varchar(30)
+    id_empleado varchar(30) primary key,
+    email varchar(60),
+    foreign key (id_empleado) references empleado(id_empleado)
 );
 
+--tratamientos a realizar
 Create table if not exists tratamiento(
-    id_tratamiento serial,
+    id_tratamiento serial primary key,
     nombre text,
     precio float
 );
 
+--inventario de la clínica
 Create table if not exists inventario(
     id_item serial primary key,
     nombre_item varchar(32),
@@ -64,15 +97,41 @@ Create table if not exists inventario(
     ultima_fecha date
 );
 
+--Cuanto inventario utiliza cada tratamiento
+Create table if not exists utilizacion_inventario(
+    id_item int,
+    cantidad int,
+    id_tratamiento int,
+    primary key(id_item, id_tratamiento),
+    foreign key (id_tratamiento) references tratamiento(id_tratamiento),
+    foreign key (id_item) references inventario(id_item)
+);
+
+--La cita, hace falta fecha y hora
 Create table if not exists cita(
     id_cita serial primary key,
     paciente int,
-    asistente varchar(30),
-    doctor varchar(30),
-    tratamiento int,
-    foreign key (paciente) references paciente(id_paciente),
-    foreign key (asistente) references asistente(nombre_usuario),
-    foreign key (doctor) references doctor(nombre_usuario),
-    foreign key (tratamiento) references tratamiento(id_tratamiento)
+    fecha date,
+    hora_inicio time,
+    hora_final time,
+    cita_activa bool,
+    foreign key (paciente) references paciente(id_paciente)
 );
 
+--tabla que relaciona las citas con los tratamientos
+Create table if not exists tratamientos_cita(
+    tratamiento int,
+    id_cita int,
+    primary key (tratamiento, id_cita),
+    foreign key (tratamiento) references tratamiento(id_tratamiento),
+    foreign key (id_cita) references cita(id_cita)
+);
+
+---participantes en la cita
+create table if not exists participantes_cita(
+    id_cita int,
+    id_empleado varchar(20),
+    primary key (id_cita, id_empleado),
+    foreign key (id_cita) references cita(id_cita),
+    foreign key (id_empleado) references empleado(id_empleado)
+    );
