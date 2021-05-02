73f0d3354e536cd21ae4fb10bdb58f2e929a3b67
Jose Hurtarte <JJHH121106@gmail.com>
2021-05-01 21:50:51 -0600

    logo in navbar

--- Frontend/src/componentes/Homescreen/Homescreen.js
+++ Frontend/src/componentes/Homescreen/Homescreen.js
@@ -10,7 +10,8 @@ function Homescreen() {
     return (
         <div>
             <header className="navbar navbar-dark sticky-top bg-dark flex-md-nowrap p-0 shadow">
-                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#"><img className = "navbarLogo"  href = {Logotipo}/></a>
+                <a className="navbar-brand col-md-3 col-lg-2 me-0 px-3" href="#">
+                    <img className = "navbarLogo"  src = {Logotipo}/></a>
                 <button className="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
@@ -118,133 +119,6 @@ function Homescreen() {
                         <canvas className="my-4 w-100" id="myChart" width="900" height="380"></canvas>
 
                         <h2>Section title</h2>
-                        <div className="table-responsive">
-                            <table className="table table-striped table-sm">
-                                <thead>
-                                    <tr>
-                                        <th>#</th>
-                                        <th>Header</th>
-                                        <th>Header</th>
-                                        <th>Header</th>
-                                        <th>Header</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    <tr>
-                                        <td>1,001</td>
-                                        <td>random</td>
-                                        <td>data</td>
-                                        <td>placeholder</td>
-                                        <td>text</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,002</td>
-                                        <td>placeholder</td>
-                                        <td>irrelevant</td>
-                                        <td>visual</td>
-                                        <td>layout</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,003</td>
-                                        <td>data</td>
-                                        <td>rich</td>
-                                        <td>dashboard</td>
-                                        <td>tabular</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,003</td>
-                                        <td>information</td>
-                                        <td>placeholder</td>
-                                        <td>illustrative</td>
-                                        <td>data</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,004</td>
-                                        <td>text</td>
-                                        <td>random</td>
-                                        <td>layout</td>
-                                        <td>dashboard</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,005</td>
-                                        <td>dashboard</td>
-                                        <td>irrelevant</td>
-                                        <td>text</td>
-                                        <td>placeholder</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,006</td>
-                                        <td>dashboard</td>
-                                        <td>illustrative</td>
-                                        <td>rich</td>
-                                        <td>data</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,007</td>
-                                        <td>placeholder</td>
-                                        <td>tabular</td>
-                                        <td>information</td>
-                                        <td>irrelevant</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,008</td>
-                                        <td>random</td>
-                                        <td>data</td>
-                                        <td>placeholder</td>
-                                        <td>text</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,009</td>
-                                        <td>placeholder</td>
-                                        <td>irrelevant</td>
-                                        <td>visual</td>
-                                        <td>layout</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,010</td>
-                                        <td>data</td>
-                                        <td>rich</td>
-                                        <td>dashboard</td>
-                                        <td>tabular</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,011</td>
-                                        <td>information</td>
-                                        <td>placeholder</td>
-                                        <td>illustrative</td>
-                                        <td>data</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,012</td>
-                                        <td>text</td>
-                                        <td>placeholder</td>
-                                        <td>layout</td>
-                                        <td>dashboard</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,013</td>
-                                        <td>dashboard</td>
-                                        <td>irrelevant</td>
-                                        <td>text</td>
-                                        <td>visual</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,014</td>
-                                        <td>dashboard</td>
-                                        <td>illustrative</td>
-                                        <td>rich</td>
-                                        <td>data</td>
-                                    </tr>
-                                    <tr>
-                                        <td>1,015</td>
-                                        <td>random</td>
-                                        <td>tabular</td>
-                                        <td>information</td>
-                                        <td>text</td>
-                                    </tr>
-                                </tbody>
-                            </table>
-                        </div>
                     </main>
                 </div>
             </div>
