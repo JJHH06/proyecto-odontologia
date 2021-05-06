b8d349a3293efedef0bea996893d0408d0bdad98
campeon19 <53914171+campeon19@users.noreply.github.com>
2021-05-05 22:06:00 -0600

    Se implemento Login

--- Frontend/src/componentes/App.js
+++ Frontend/src/componentes/App.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import Login from './Login/Login';
+import './App.css';
 import 'bootstrap/dist/css/bootstrap.css';
 import 'bootstrap/dist/js/bootstrap';
 import Homescreen from './Homescreen/Homescreen';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
-class App extends React.Component{
-    render(){
-        return(
-            <div className='app'>
-                <Homescreen/>
-            </div>
-        );
+function App(){
+    const [token, setToken] = useState();
+
+    if (!token) {
+      return <Login setToken={setToken} />;
     }
+    
+    return(
+        <div className='app'>
+            <BrowserRouter>
+                <Switch>
+                    <Route path="/">
+                        <Homescreen />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        </div>
+        );
+    
 }
 
 export default App
--- Frontend/src/componentes/Login/Login.js
+++ Frontend/src/componentes/Login/Login.js
@@ -1,18 +1,78 @@
 /* eslint-disable prefer-template */
 import React, { useState } from 'react';
-import './Login.css';
+import 'bootstrap';
+import './Login.scss';
+import Logotipo from '../../assets/FULL_COLOR.png';
+import PropTypes from 'prop-types';
 
-// objeto de la calculadora
-function Login() {
+function Login({setToken}){
+  const [username, setUserName] = useState();
+  const [password, setPassword] = useState();
 
+  const handleSubmit = () => {
+    const token = 'token123';
+    if(username == 'christiandp@live.com' && password == '123456789'){
+      setToken(token);
+    }
+  }
   return (
-    <form className='box'>
-        <h1>Login</h1>
+    <div className="login">
+      <div className="container">
+        <div className="row content">
+          <div className="col-md-6 mb-3">
+            <img src={Logotipo} className="img-fluid posicion" alt="imagen" />
+          </div>
+          <div className="col-md-6">
+            <h3 className="signin-text mb-3">Inicio de Sesion</h3>
+            <form onSubmit={handleSubmit}>
+              <div className="form-group">
+                <label htmlFor="email">Email</label>
+                <input type="email" name="email" className="form-control" onChange={e =>setUserName(e.target.value)} />
+              </div>
+              <div className="form-group">
+                <label htmlFor="password" className="espacio">
+                  Password
+                </label>
+                <input
+                  type="password"
+                  name="password"
+                  className="form-control"
+                  onChange={e => setPassword(e.target.value)}
+                />
+              </div>
+              <div className="form-group form-check">
+                <input
+                  type="checkbox"
+                  name="checkbox"
+                  className="form-check-input"
+                />
+                <label className="form-check-label" htmlFor="checkbox">
+                  Remember Me
+                </label>
+              </div>
+              <input
+                type="submit"
+                name="submit"
+                value="Iniciar sesion"
+                className="btn btn-class"
+              />
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Login;
+
+Login.propTypes = {
+  setToken: PropTypes.func.isRequired
+}
+
+
+/*
         <input type='text' name='' placeholder='Username' />
         <input type='password' name='' placeholder= 'Password' />
         <input type='submit' name='' value= 'Login' />
-    </form>
-  );
-}
-
-export default Login;
+*/
