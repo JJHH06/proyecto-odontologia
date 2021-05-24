b540e86815d145c17dc26de68e95360c0adbba6b
Jose Hurtarte <JJHH121106@gmail.com>
2021-05-23 21:02:36 -0600

    login connection database

--- Frontend/src/componentes/Login/Login.js
+++ Frontend/src/componentes/Login/Login.js
@@ -4,16 +4,82 @@ import 'bootstrap';
 import './Login.scss';
 import Logotipo from '../../assets/FULL_COLOR.png';
 import PropTypes from 'prop-types';
+import axios from 'axios';
+
+// const axios = require('axios');
+
+// function loginConnection (email, pass){
+//   var data = JSON.stringify({
+//     "id_attemp": email,
+//     "pass_attemp": pass
+//   });
+//   let respuesta = {}
+  
+//   var config = {
+//     method: 'post',
+//     url: 'http://localhost:5000/api/login/validate_login',
+//     headers: { 
+//       'Content-Type': 'application/json'
+//     },
+//     data : data
+//   };
+  
+//   axios(config)
+//   .then(function (response) {
+//     respuesta = JSON.stringify(response.data)
+//     console.log(JSON.stringify(response.data).validate_login);
+//     //return JSON.stringify(JSON.stringify(response.data).validate_login)
+    
+    
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   });
+// }
+
+//loginConnection("XD","XDDDD")
+
+
 
 function Login({setToken}){
   const [username, setUserName] = useState();
   const [password, setPassword] = useState();
 
-  const handleSubmit = () => {
+  const handleSubmit = (e) => {
+    e.preventDefault()
     const token = 'token123';
-    if(username == 'proyecto@gmail.com' && password == '123456789'){
+
+
+    
+  var data = JSON.stringify({
+    "id_attemp": username,
+    "pass_attemp": password
+  });
+  
+  var config = {
+    method: 'post',
+    url: 'http://localhost:5000/api/login/validate_login',
+    headers: { 
+      'Content-Type': 'application/json'
+    },
+    data : data
+  };
+  axios(config)
+  .then(function (response) {
+    console.log(JSON.stringify(response.data));
+    if (JSON.stringify(response.data)["validate_login"]){
       setToken(token);
     }
+    
+  })
+  .catch(function (error) {
+    console.log(error);
+  });
+
+
+
+
+
   }
   return (
     <div className="login">
