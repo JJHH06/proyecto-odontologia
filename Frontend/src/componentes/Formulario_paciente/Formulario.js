2ae475603375e80471e769764155448653e09426
campeon19 <53914171+campeon19@users.noreply.github.com>
2021-05-24 13:39:47 -0600

    cambio tel

--- Frontend/src/componentes/Formulario_paciente/Formulario.js
+++ Frontend/src/componentes/Formulario_paciente/Formulario.js
@@ -4,8 +4,20 @@ import './Formulario.scss';
 function Formulario() {
 
     return (
-        <div>
-            
+        <div className='formulario-paciente'>
+            <div className='container'>
+                <h1>Ficha Nuevo Paciente</h1>
+                <form>
+                    <div className=''>
+                        <label htmlFor='nombre'>Nombre</label>
+                        <input type="text" name="nombre" className="form-control"/>
+                    </div>
+                    <div>
+                        <label htmlFor='telefono_casa'>Nombre</label>
+                        <input type="tel" name="telefono_casa" pattern="[0-9]{4}-[0-9]{4}" className="form-control"/>
+                    </div>
+                </form>
+            </div>
         </div>
     )
 }
