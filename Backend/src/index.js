90e11d24897863813a0822f2b8be59d2d6385559
Hansel López <halm29799@gmail.com>
2021-05-25 00:56:12 -0600

    refactor: Se instaló nodemon y ligeras mejoras en Index
    Nota: Para iniciar server con nodemon usar 
    npm run dev

--- Backend/src/index.js
+++ Backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const path = require("path");
 
 //settings
 app.set('port', process.env.PORT || 5000);
@@ -11,11 +12,11 @@ app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 
 //routes
-app.use('/api/paciente', require('./routes/paciente'));
-app.use('/api/empleado', require('./routes/empleado'));
-app.use('/api/login', require('./routes/login'));
+app.use('/api/paciente', require(path.join(__dirname, '/routes/paciente')));
+app.use('/api/empleado', require(path.join(__dirname, '/routes/empleado')));
+app.use('/api/login', require(path.join(__dirname, '/routes/login')));
 
 //starting the server
 app.listen(app.get('port'), () => {
-    console.log(`Server on port ${app.get('port')}`);
+    console.log(` The server has started successfully on port: ${app.get('port')} !!`);
 });
