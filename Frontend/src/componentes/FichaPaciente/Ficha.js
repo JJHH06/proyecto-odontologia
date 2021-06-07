ad879e1ade6381d0c99f563d7629ddf3ceda5596
campeon19 <53914171+campeon19@users.noreply.github.com>
2021-06-06 22:09:25 -0600

    mas cosas ficha medica

--- Frontend/src/componentes/FichaPaciente/Ficha.js
+++ Frontend/src/componentes/FichaPaciente/Ficha.js
@@ -36,6 +36,35 @@ function Ficha({props}) {
                         </ul>
                     </div>
                 </div>
+                <div className='col-lg-12'>
+                    <div className='cuadro'>
+                        <div className='container-h3 grande'>
+                            <h3>Historial Medico</h3>
+                        </div>
+                        <ul>
+                            <li><p className='subtitulo'>Recomendado Por: <span>Jose carlos</span></p> </li>
+                            <li><p className='subtitulo'>Visita anterior al dentista: <span>01/01/2021</span></p> </li>
+                            <li><p className='subtitulo'>Medico: <span>Dr simi</span></p> </li>
+                            <li><p className='subtitulo'>Telefono medico: <span>1111-1111</span></p> </li>
+                            <li><p className='subtitulo'>Contacto de emergencia: <span>elpepe</span></p> </li>
+                            <li><p className='subtitulo'>Telefono medico: <span>1777-1777</span></p> </li>
+                            
+                        </ul>
+                    </div>
+                </div>
+                <div className='col-lg-12'>
+                    <div className='cuadro'>
+                        <div className='container-h3'>
+                            <h3>Cita</h3>
+                        </div>
+                        <ul>
+                            <li><p className='subtitulo'>Motivo de consulta: <span>Dolor de muela</span></p> </li>
+                            <li><p className='subtitulo'>Presupuesto: <span>No calculado</span></p> </li>
+                            <li><p className='subtitulo'>Medicamentos: <span>Ninguno</span></p> </li>
+                            
+                        </ul>
+                    </div>
+                </div>
                 
             </div>
         </div>
