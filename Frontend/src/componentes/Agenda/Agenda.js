d0754e545ee4eb44db39498cfe6e7696d4951260
campeon19 <53914171+campeon19@users.noreply.github.com>
2021-07-24 00:08:07 -0600

    Mejoras en calendario con respecto a eventos
    Ahora se puede ingresar y eliminar eventos

--- Frontend/src/componentes/Agenda/Agenda.js
+++ Frontend/src/componentes/Agenda/Agenda.js
@@ -4,8 +4,10 @@ import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import timeGridPlugin from '@fullcalendar/timegrid'
 import interactionPlugin from '@fullcalendar/interaction'
+import listPlugin from '@fullcalendar/list';
+import { INITIAL_EVENTS, createEventId } from './event-utils'
 
-export default class DemoApp extends React.Component {
+export default class Agenda extends React.Component {
     
     state = {
         weekendsVisible: true,
@@ -16,21 +18,73 @@ export default class DemoApp extends React.Component {
     return (
         <div className='container size-calendar'>
             <FullCalendar
-                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin ]}
+                plugins={[ dayGridPlugin, timeGridPlugin, interactionPlugin, listPlugin ]}
                 headerToolbar={{
                     left: 'prev,next today',
                     center: 'title',
-                    right: 'dayGridMonth,timeGridWeek,timeGridDay'
+                    right: 'dayGridMonth,timeGridWeek,timeGridDay,listWeek'
                 }}
-                initialView="dayGridMonth"
-                initialView='dayGridMonth'
+                initialView="timeGridDay"
                 editable={true}
                 selectable={true}
+                selectHelper={true}
                 selectMirror={true}
                 dayMaxEvents={true}
+                eventLimit={true}
                 weekends={this.state.weekendsVisible}
+                initialEvents={INITIAL_EVENTS} // alternatively, use the `events` setting to fetch from a feed
+                select={this.handleDateSelect}
+                eventContent={renderEventContent} // custom render function
+                eventClick={this.handleEventClick}
+                eventsSet={this.handleEvents} 
+
             />
         </div>
     )
   }
+
+  handleWeekendsToggle = () => {
+    this.setState({
+      weekendsVisible: !this.state.weekendsVisible
+    })
+  }
+
+  handleDateSelect = (selectInfo) => {
+    let title = prompt('Please enter a new title for your event')
+    let calendarApi = selectInfo.view.calendar
+
+    calendarApi.unselect() // clear date selection
+
+    if (title) {
+      calendarApi.addEvent({
+        id: createEventId(),
+        title,
+        start: selectInfo.startStr,
+        end: selectInfo.endStr,
+        allDay: selectInfo.allDay
+      })
+    }
+  }
+
+  handleEventClick = (clickInfo) => {
+    if (window.confirm(`Are you sure you want to delete the event '${clickInfo.event.title}'`)) {
+      clickInfo.event.remove()
+    }
+  }
+
+  handleEvents = (events) => {
+    this.setState({
+      currentEvents: events
+    })
+  }
+
+}
+
+function renderEventContent(eventInfo) {
+    return (
+      <>
+        <b>{eventInfo.timeText}</b>
+        <i>{eventInfo.event.title}</i>
+      </>
+    )
 }
