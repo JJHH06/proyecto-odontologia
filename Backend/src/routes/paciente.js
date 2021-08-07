1d6f2bd6d2410e37449b4bcea080cfa5dbafb9d5
Hansel López <halm29799@gmail.com>
2021-08-07 16:08:27 -0600

    fix formato fecha paciente.js
    -addPaciente
    -searchPaciente
    -getAllPacientes
    -getPaciente
    -updatePaciente
    -deletePaciente

--- Backend/src/routes/paciente.js
+++ Backend/src/routes/paciente.js
@@ -1,13 +1,13 @@
-const {Router} = require('express');
+const { Router } = require('express');
 const router = Router();
 const pool = require("./db");
 
-router.post("/addPaciente", async(req,res) => {
+router.post("/addPaciente", async (req, res) => {
     try {
-        const {nombre,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,abonos} = req.body;
+        const { nombre, email, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos } = req.body;
         const addPaciente = await pool.query(
-            "INSERT INTO paciente(nombre,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,abonos) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) RETURNING *",
-            [nombre, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, abonos]
+            "INSERT INTO paciente(nombre,email,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,presupuesto,medicamentos) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) RETURNING *",
+            [nombre, email, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos]
         );
         res.json(addPaciente.rows[0]);
     } catch (err) {
@@ -15,9 +15,9 @@ router.post("/addPaciente", async(req,res) => {
     }
 });
 
-router.post("/searchPaciente", async(req,res) => {
+router.post("/searchPaciente", async (req, res) => {
     try {
-        const {busqueda} = req.body;
+        const { busqueda } = req.body;
         const response = await pool.query(
             `
             select *, Extract(year from age(fecha_nacimiento)) as edad, getPadecimientos(id_paciente) as padecimientos from paciente
@@ -25,50 +25,110 @@ router.post("/searchPaciente", async(req,res) => {
             `,
             [busqueda]
         )
-        console.log(response.rows);
+
+        for (var i = 0; i < response.rows.length; i++) {
+            var date = response.rows[i].fecha_nacimiento.toString();
+            date = new Date(date).toISOString().substr(0, 10);
+            var datePart = date.match(/\d+/g);
+            year = datePart[0],
+                month = datePart[1], day = datePart[2]
+            response.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+        }
+
+        for (var i = 0; i < response.rows.length; i++) {
+            var date = response.rows[i].visita_anterior_dentista.toString();
+            year = new Date(date).getFullYear();
+            date = new Date(date);
+
+            var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
+            var fechaFinal = month + ', ' + year;
+            response.rows[i].visita_anterior_dentista = fechaFinal;
+        }
+
+        //console.log(response.rows);
         res.json(response.rows);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-router.get("/getAllPacientes", async(req,res) => {
+router.get("/getAllPacientes", async (req, res) => {
     try {
         const getAllPacientes = await pool.query(
-            `SELECT id_paciente, nombre, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, abonos 
+            `SELECT * 
             FROM paciente`
         )
+
+        for (var i = 0; i < getAllPacientes.rows.length; i++) {
+            var date = getAllPacientes.rows[i].fecha_nacimiento.toString();
+            date = new Date(date).toISOString().substr(0, 10);
+            var datePart = date.match(/\d+/g);
+            year = datePart[0],
+                month = datePart[1], day = datePart[2]
+            getAllPacientes.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+        }
+
+        for (var i = 0; i < getAllPacientes.rows.length; i++) {
+            var date = getAllPacientes.rows[i].visita_anterior_dentista.toString();
+            year = new Date(date).getFullYear();
+            date = new Date(date);
+
+            var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
+            var fechaFinal = month + ', ' + year;
+            getAllPacientes.rows[i].visita_anterior_dentista = fechaFinal;
+        }
+
         res.json(getAllPacientes.rows);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-router.get("/getPaciente", async(req,res) => {
+router.get("/getPaciente", async (req, res) => {
     try {
-        const {id_paciente} = req.body;
+        const { id_paciente } = req.body;
         const getPaciente = await pool.query(
-            `SELECT id_paciente, nombre, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, abonos 
+            `SELECT * 
             FROM paciente WHERE id_paciente = $1`,
             [id_paciente]
         );
+
+        for (var i = 0; i < getPaciente.rows.length; i++) {
+            var date = getPaciente.rows[i].fecha_nacimiento.toString();
+            date = new Date(date).toISOString().substr(0, 10);
+            var datePart = date.match(/\d+/g);
+            year = datePart[0],
+                month = datePart[1], day = datePart[2]
+            getPaciente.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+        }
+
+        for (var i = 0; i < getPaciente.rows.length; i++) {
+            var date = getPaciente.rows[i].visita_anterior_dentista.toString();
+            year = new Date(date).getFullYear();
+            date = new Date(date);
+
+            var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
+            var fechaFinal = month + ', ' + year;
+            getPaciente.rows[i].visita_anterior_dentista = fechaFinal;
+        }
+
         res.json(getPaciente.rows[0]);
     } catch (err) {
         console.error(err.message);
     }
 });
 
-router.put("/updatePaciente", async(req,res) => {
+router.put("/updatePaciente", async (req, res) => {
     try {
-        const {id_paciente} = req.body;
-        const {nombre,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,abonos} = req.body;
+        const { id_paciente } = req.body;
+        const { nombre, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos } = req.body;
         const updatePaciente = await pool.query(
-            `UPDATE paciente SET nombre=$1, telefono_casa=$2, telefono_celular=$3, fecha_nacimiento=$4, estado_civil=$5, ocupacion=$6, direccion=$7, recomendado_por=$8, visita_anterior_dentista=$9, motivo_consulta=$10, medico=$11, telefono_medico=$12, contacto_emergencia=$13, telefono_emergencia=$14, abonos=$15
-            WHERE id_paciente = $16`,
-            [nombre,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,abonos,id_paciente]
+            `UPDATE paciente SET nombre=$1, telefono_casa=$2, telefono_celular=$3, fecha_nacimiento=$4, estado_civil=$5, ocupacion=$6, direccion=$7, recomendado_por=$8, visita_anterior_dentista=$9, motivo_consulta=$10, medico=$11, telefono_medico=$12, contacto_emergencia=$13, telefono_emergencia=$14, presupuesto=$15, medicamentos=$16
+            WHERE id_paciente = $17`,
+            [nombre, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos, id_paciente]
         );
         const getPaciente = await pool.query(
-            `SELECT id_paciente, nombre, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, abonos 
+            `SELECT * 
             FROM paciente WHERE id_paciente = $1`,
             [id_paciente]
         );
@@ -78,9 +138,9 @@ router.put("/updatePaciente", async(req,res) => {
     }
 });
 
-router.delete("/deletePaciente", async(req,res) => {
+router.delete("/deletePaciente", async (req, res) => {
     try {
-        const {id_paciente} = req.body;
+        const { id_paciente } = req.body;
         const deletePaciente = await pool.query(
             `DELETE FROM paciente WHERE id_paciente = $1`,
             [id_paciente]
