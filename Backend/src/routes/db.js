410a6f32821d5b796b4745f2a78a47b88f097259
Hansel López <halm29799@gmail.com>
2021-08-21 23:16:56 -0600

    add dotenv

--- Backend/src/routes/db.js
+++ Backend/src/routes/db.js
@@ -1,11 +1,12 @@
 const Pool = require("pg").Pool;
+require('dotenv').config();
 
 const pool = new Pool({
-    user: "cliente",
-    password: "password",
-    host: "143.198.170.87",
-    port: 5432,
-    database: "odontologia"
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    database: process.env.DB_DATABASE
 });
 
 module.exports = pool;
