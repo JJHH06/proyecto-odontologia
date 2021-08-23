6f1cb72ea0cd43cd84ffb559fde751c64c53f2d5
Jose Hurtarte <JJHH121106@gmail.com>
2021-08-22 23:51:40 -0600

    Integration test

--- Frontend/src/componentes/FichaPaciente/__test__/Ficha.test.js
+++ Frontend/src/componentes/FichaPaciente/__test__/Ficha.test.js
@@ -3,28 +3,28 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import {render, cleanup, screen} from '@testing-library/react';
 import "@testing-library/jest-dom/extend-expect";
-import Formulario from '../Formulario';
+import Ficha from '../../Ficha_paciente/Ficha';
 
 
 
 afterEach(cleanup);
 it("Renders without crashing",()=>{
     const div = document.createElement("div");
-    ReactDOM.render(<Formulario/>, div);
+    ReactDOM.render(<Ficha/>, div);
     ReactDOM.unmountComponentAtNode(div);
 });
 
-it("Renders Formulario Correctly",() =>{
-    const {getByTestId}= render(<Formulario/>)
+it("Renders Ficha Correctly",() =>{
+    const {getByTestId}= render(<Ficha/>)
     expect(getByTestId('patient-form')).toHaveTextContent("Paciente")
 });
 
-it('Formulario result is displayed', async () => {
-    render(<Formulario/>);
+it('Ficha result is displayed', async () => {
+    render(<Ficha/>);
     expect(screen.getByTestId('patient-form')).toBeInTheDocument();
 });
 
 it("Matches snapshot",() =>{
-    const tree = renderer.create(<Formulario/>).toJSON();
+    const tree = renderer.create(<Ficha/>).toJSON();
     expect(tree).toMatchSnapshot();
 })
