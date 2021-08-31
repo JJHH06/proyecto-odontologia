16a8db266129dbd5c1052075e4342eb1f45b0fc4
Hansel López <halm29799@gmail.com>
2021-08-31 15:56:54 -0600

    Fix addPaciente
    Ahora se permite insertar fechas null

--- Backend/src/routes/paciente.js
+++ Backend/src/routes/paciente.js
@@ -5,12 +5,39 @@ const pool = require("./db");
 router.post("/addPaciente", async (req, res) => {
     try {
         const { nombre, email, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos } = req.body;
-        const addPaciente = await pool.query(
-            "INSERT INTO paciente(nombre,email,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,presupuesto,medicamentos) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) RETURNING *",
-            [nombre, email, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos]
-        );
-        //res.json(addPaciente.rows[0]);
-        res.status(200).send({ code: 1, result: addPaciente.rows[0] });
+        //console.log(req.body)
+
+        var RegExPattern = /^\d{1,2}\/\d{1,2}\/\d{2,4}$/;
+        if ((fecha_nacimiento.match(RegExPattern)) && (fecha_nacimiento != '')) {
+
+            if ((visita_anterior_dentista.match(RegExPattern)) && (visita_anterior_dentista != '')) {
+                const addPaciente = await pool.query(
+                    "INSERT INTO paciente(nombre,email,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,presupuesto,medicamentos) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16,$17) RETURNING *",
+                    [nombre, email, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos]
+                );
+                res.status(200).send({ code: 1, result: addPaciente.rows[0] });
+            } else {
+                const addPaciente = await pool.query(
+                    "INSERT INTO paciente(nombre,email,telefono_casa,telefono_celular,fecha_nacimiento,estado_civil,ocupacion,direccion,recomendado_por,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,presupuesto,medicamentos) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16) RETURNING *",
+                    [nombre, email, telefono_casa, telefono_celular, fecha_nacimiento, estado_civil, ocupacion, direccion, recomendado_por, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos]
+                );
+                res.status(200).send({ code: 1, result: addPaciente.rows[0] });
+            }
+        } else {
+            if ((visita_anterior_dentista.match(RegExPattern)) && (visita_anterior_dentista != '')) {
+                const addPaciente = await pool.query(
+                    "INSERT INTO paciente(nombre,email,telefono_casa,telefono_celular,estado_civil,ocupacion,direccion,recomendado_por,visita_anterior_dentista,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,presupuesto,medicamentos) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15,$16) RETURNING *",
+                    [nombre, email, telefono_casa, telefono_celular, estado_civil, ocupacion, direccion, recomendado_por, visita_anterior_dentista, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos]
+                );
+                res.status(200).send({ code: 1, result: addPaciente.rows[0] });
+            } else {
+                const addPaciente = await pool.query(
+                    "INSERT INTO paciente(nombre,email,telefono_casa,telefono_celular,estado_civil,ocupacion,direccion,recomendado_por,motivo_consulta,medico,telefono_medico,contacto_emergencia,telefono_emergencia,presupuesto,medicamentos) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15) RETURNING *",
+                    [nombre, email, telefono_casa, telefono_celular, estado_civil, ocupacion, direccion, recomendado_por, motivo_consulta, medico, telefono_medico, contacto_emergencia, telefono_emergencia, presupuesto, medicamentos]
+                );
+                res.status(200).send({ code: 1, result: addPaciente.rows[0] });
+            }
+        }
     } catch (err) {
         console.error(err.message);
         res.status(200).send({ code: 0, error: err.message });
@@ -29,22 +56,26 @@ router.post("/searchPaciente", async (req, res) => {
         )
 
         for (var i = 0; i < response.rows.length; i++) {
-            var date = response.rows[i].fecha_nacimiento.toString();
-            date = new Date(date).toISOString().substr(0, 10);
-            var datePart = date.match(/\d+/g);
-            year = datePart[0],
-                month = datePart[1], day = datePart[2]
-            response.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+            if (response.rows[i].fecha_nacimiento != null) {
+                var date = response.rows[i].fecha_nacimiento.toString();
+                date = new Date(date).toISOString().substr(0, 10);
+                var datePart = date.match(/\d+/g);
+                year = datePart[0],
+                    month = datePart[1], day = datePart[2]
+                response.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+            }
         }
 
         for (var i = 0; i < response.rows.length; i++) {
-            var date = response.rows[i].visita_anterior_dentista.toString();
-            year = new Date(date).getFullYear();
-            date = new Date(date);
+            if (response.rows[i].visita_anterior_dentista != null) {
+                var date = response.rows[i].visita_anterior_dentista.toString();
+                year = new Date(date).getFullYear();
+                date = new Date(date);
 
-            var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
-            var fechaFinal = month + ', ' + year;
-            response.rows[i].visita_anterior_dentista = fechaFinal;
+                var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
+                var fechaFinal = month + ', ' + year;
+                response.rows[i].visita_anterior_dentista = fechaFinal;
+            }
         }
 
         //console.log(response.rows);
@@ -65,22 +96,26 @@ router.get("/getAllPacientes", async (req, res) => {
         )
 
         for (var i = 0; i < getAllPacientes.rows.length; i++) {
-            var date = getAllPacientes.rows[i].fecha_nacimiento.toString();
-            date = new Date(date).toISOString().substr(0, 10);
-            var datePart = date.match(/\d+/g);
-            year = datePart[0],
-                month = datePart[1], day = datePart[2]
-            getAllPacientes.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+            if (getAllPacientes.rows[i].fecha_nacimiento != null) {
+                var date = getAllPacientes.rows[i].fecha_nacimiento.toString();
+                date = new Date(date).toISOString().substr(0, 10);
+                var datePart = date.match(/\d+/g);
+                year = datePart[0],
+                    month = datePart[1], day = datePart[2]
+                getAllPacientes.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+            }
         }
 
         for (var i = 0; i < getAllPacientes.rows.length; i++) {
-            var date = getAllPacientes.rows[i].visita_anterior_dentista.toString();
-            year = new Date(date).getFullYear();
-            date = new Date(date);
+            if (getAllPacientes.rows[i].visita_anterior_dentista != null) {
+                var date = getAllPacientes.rows[i].visita_anterior_dentista.toString();
+                year = new Date(date).getFullYear();
+                date = new Date(date);
 
-            var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
-            var fechaFinal = month + ', ' + year;
-            getAllPacientes.rows[i].visita_anterior_dentista = fechaFinal;
+                var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
+                var fechaFinal = month + ', ' + year;
+                getAllPacientes.rows[i].visita_anterior_dentista = fechaFinal;
+            }
         }
 
         res.status(200).send({ code: 1, result: getAllPacientes.rows });
@@ -101,22 +136,26 @@ router.get("/getPaciente", async (req, res) => {
         );
 
         for (var i = 0; i < getPaciente.rows.length; i++) {
-            var date = getPaciente.rows[i].fecha_nacimiento.toString();
-            date = new Date(date).toISOString().substr(0, 10);
-            var datePart = date.match(/\d+/g);
-            year = datePart[0],
-                month = datePart[1], day = datePart[2]
-            getPaciente.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+            if (getPaciente.rows[i].fecha_nacimiento != null) {
+                var date = getPaciente.rows[i].fecha_nacimiento.toString();
+                date = new Date(date).toISOString().substr(0, 10);
+                var datePart = date.match(/\d+/g);
+                year = datePart[0],
+                    month = datePart[1], day = datePart[2]
+                getPaciente.rows[i].fecha_nacimiento = day + '/' + month + '/' + year;
+            }
         }
 
         for (var i = 0; i < getPaciente.rows.length; i++) {
-            var date = getPaciente.rows[i].visita_anterior_dentista.toString();
-            year = new Date(date).getFullYear();
-            date = new Date(date);
+            if (getPaciente.rows[i].visita_anterior_dentista != null) {
+                var date = getPaciente.rows[i].visita_anterior_dentista.toString();
+                year = new Date(date).getFullYear();
+                date = new Date(date);
 
-            var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
-            var fechaFinal = month + ', ' + year;
-            getPaciente.rows[i].visita_anterior_dentista = fechaFinal;
+                var month = new Intl.DateTimeFormat('es-ES', { month: 'long' }).format(date);
+                var fechaFinal = month + ', ' + year;
+                getPaciente.rows[i].visita_anterior_dentista = fechaFinal;
+            }
         }
 
         //res.json(getPaciente.rows[0]);
