ebcc4800885fadea43102965ac0b8fb7b38602dd
Hansel López <halm29799@gmail.com>
2021-10-16 19:36:31 -0600

    add getToken

--- Backend/src/routes/login.js
+++ Backend/src/routes/login.js
@@ -1,6 +1,8 @@
 const { Router } = require('express');
 const router = Router();
 const pool = require("./db");
+const jwt = require("jsonwebtoken");
+
 
 router.post("/validate_login", async (req, res) => {
     try {
@@ -10,7 +12,21 @@ router.post("/validate_login", async (req, res) => {
             [id_attemp, pass_attemp]
         );
         //res.json(validate_login.rows[0]);
-        res.status(200).send({ code: 1, result: validate_login.rows[0] });
+        const user = {
+            id_attemp,
+            pass_attemp
+        }
+        //console.log(validate_login.rows[0].validate_login)
+        if (validate_login.rows[0].validate_login) {
+            jwt.sign({ user }, 'secretKey', { expiresIn: "60s" }, (err, token) => {
+                validate_login.rows[0].token = token;
+                res.status(200).send({ code: 1, result: validate_login.rows[0] });
+            });
+        } else {
+            res.status(200).send({ code: 1, result: validate_login.rows[0] });
+        }
+
+
     } catch (err) {
         console.error(err.message);
         res.status(200).send({ code: 0, error: err.message });
