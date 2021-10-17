872daeb357118151a6fc6641147ec5ae9940be12
Hansel López <halm29799@gmail.com>
2021-10-16 23:10:22 -0600

    add tratamiento_paciente Authentication

--- Backend/src/routes/tratamiento_paciente.js
+++ Backend/src/routes/tratamiento_paciente.js
@@ -1,8 +1,22 @@
 const { Router } = require('express');
 const router = Router();
 const pool = require("./db");
+const jwt = require("jsonwebtoken");
 
-router.post("/getTratamientoByPieza", async (req, res) => {
+function verifyToken(req, res, next) {
+    const bearerHeader = req.headers['authorization'];
+    if (typeof bearerHeader !== 'undefined') {
+        const bearer = bearerHeader.split(' ');
+        const bearerToken = bearer[2];
+        req.token = bearerToken;
+        //console.log("TOKEN ", req.token)
+        next();
+    } else {
+        res.sendStatus(401);
+    }
+}
+
+router.post("/getTratamientoByPieza", verifyToken, async (req, res) => {
     try {
         const { no_pieza, id_paciente } = req.body;
         const getTratamientoByPieza = await pool.query(
@@ -11,14 +25,23 @@ router.post("/getTratamientoByPieza", async (req, res) => {
             and tp.no_pieza::integer = $1 and tp.id_paciente::integer = $2`,
             [no_pieza, id_paciente]
         );
-        res.status(200).send({ code: 1, result: getTratamientoByPieza.rows });
+        jwt.verify(req.token, 'secretKey', (error, authData) => {
+            //console.log("token", req.token, "token")
+            if (error) {
+                console.log("error", error)
+                res.sendStatus(401);
+            } else {
+                res.status(200).send({ code: 1, result: getTratamientoByPieza.rows });
+            }
+        });
+        //res.status(200).send({ code: 1, result: getTratamientoByPieza.rows });
     } catch (err) {
         console.error(err.message)
         res.status(200).send({ code: 0, error: err.message });
     }
 });
 
-router.post("/getTratamientos", async (req, res) => {
+router.post("/getTratamientos", verifyToken, async (req, res) => {
     try {
         const { id_paciente } = req.body;
         const getTratamientos = await pool.query(
@@ -26,7 +49,16 @@ router.post("/getTratamientos", async (req, res) => {
             on t.id_tratamiento = tp.id_tratamiento where id_paciente = $1 group by t.id_tratamiento, t.nombre`,
             [id_paciente]
         );
-        res.status(200).send({ code: 1, result: getTratamientos.rows });
+        jwt.verify(req.token, 'secretKey', (error, authData) => {
+            //console.log("token", req.token, "token")
+            if (error) {
+                console.log("error", error)
+                res.sendStatus(401);
+            } else {
+                res.status(200).send({ code: 1, result: getTratamientos.rows });
+            }
+        });
+        //res.status(200).send({ code: 1, result: getTratamientos.rows });
 
     } catch (err) {
         console.error(err.message)
