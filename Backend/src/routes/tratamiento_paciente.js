9d6087bda865d91e6f0f470b6562defc725c6059
Hansel López <halm29799@gmail.com>
2021-11-05 10:31:25 -0600

    update and add tratamiento_paciente routes
    -add addTratamiento Route
    -add deleteTratamiento Route
    -update getTratamientoByPieza Route: ahora se retorna id_tratamiento_paciente y id_tratamiento

--- Backend/src/routes/tratamiento_paciente.js
+++ Backend/src/routes/tratamiento_paciente.js
@@ -20,7 +20,7 @@ router.post("/getTratamientoByPieza", verifyToken, async (req, res) => {
     try {
         const { no_pieza, id_paciente } = req.body;
         const getTratamientoByPieza = await pool.query(
-            `select no_pieza, tp.seccion, t.nombre as nombre_tratamiento from tratamientos_paciente tp
+            `select tp.id_tratamiento_paciente, tp.id_tratamiento, no_pieza, tp.seccion, t.nombre as nombre_tratamiento from tratamientos_paciente tp
             inner join tratamiento t on t.id_tratamiento = tp.id_tratamiento
             and tp.no_pieza::integer = $1 and tp.id_paciente::integer = $2`,
             [no_pieza, id_paciente]
@@ -85,6 +85,53 @@ router.post("/getTratamientos2", verifyToken, async (req, res) => {
         });
         //res.status(200).send({ code: 1, result: getTratamientos.rows });
 
+    } catch (err) {
+        console.error(err.message)
+        res.status(200).send({ code: 0, error: err.message });
+    }
+});
+
+router.post("/addTratamiento", verifyToken, async (req, res) => {
+    try {
+        const { idTratamiento, noPieza, idPaciente, seccion } = req.body;
+        const addTratamiento = await pool.query(
+            `INSERT INTO tratamientos_paciente (id_tratamiento, no_pieza, id_paciente, seccion)
+            VALUES ($1, $2, $3, $4) RETURNING *`,
+            [idTratamiento, noPieza, idPaciente, seccion]
+        );
+
+        jwt.verify(req.token, 'secretKey', (error, authData) => {
+            //console.log("token", req.token, "token")
+            if (error) {
+                console.log("error", error)
+                res.sendStatus(401);
+            } else {
+                res.status(200).send({ code: 1, result: addTratamiento.rows[0] });
+            }
+        });
+    } catch (err) {
+        console.error(err.message)
+        res.status(200).send({ code: 0, error: err.message });
+    }
+});
+
+router.delete("/deleteTratamiento", verifyToken, async (req, res) => {
+    try {
+        const { idTratamientoPaciente } = req.body;
+        const deleteTratamiento = await pool.query(
+            `DELETE FROM tratamientos_paciente WHERE id_tratamiento_paciente = $1`,
+            [idTratamientoPaciente]
+        );
+
+        jwt.verify(req.token, 'secretKey', (error, authData) => {
+            //console.log("token", req.token, "token")
+            if (error) {
+                console.log("error", error)
+                res.sendStatus(401);
+            } else {
+                res.status(200).send({ code: 1, result: "Tratamiento was deleted!" });
+            }
+        });
     } catch (err) {
         console.error(err.message)
         res.status(200).send({ code: 0, error: err.message });
