c2aa493fd01d8c5d599502e926893dcb097f1977
Hansel López <halm29799@gmail.com>
2021-11-21 18:17:17 -0600

    update addEmpleado updateEmplado and security vulnerabilities were fixed

--- Backend/src/routes/empleado.js
+++ Backend/src/routes/empleado.js
@@ -19,18 +19,18 @@ function verifyToken(req, res, next) {
 
 router.post("/addEmpleado", verifyToken, async (req, res) => {
     try {
-        const { id_empleado, password, nombre } = req.body;
-        const addEmpleado = await pool.query(
-            "INSERT INTO empleado(id_empleado, password, nombre) VALUES($1,$2,$3) RETURNING *",
-            [id_empleado, password, nombre]
-        );
-        //res.json(addEmpleado.rows[0]);
-        jwt.verify(req.token, process.env.SECRET_KEY, (error, authData) => {
+        jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
             //console.log("token", req.token, "token")
             if (error) {
                 console.log("error", error)
                 res.sendStatus(401);
             } else {
+                const { id_empleado, password, nombre, email, tipo } = req.body;
+                const addEmpleado = await pool.query(
+                    "INSERT INTO empleado(id_empleado, password, nombre, email, tipo) VALUES($1,$2,$3,$4,$5) RETURNING *",
+                    [id_empleado, password, nombre, email, tipo]
+                );
+                //res.json(addEmpleado.rows[0]);
                 res.status(200).send({ code: 1, result: addEmpleado.rows[0] });
             }
         });
@@ -45,16 +45,16 @@ router.post("/addEmpleado", verifyToken, async (req, res) => {
 
 router.get("/getAllEmpleados", verifyToken, async (req, res) => {
     try {
-        const getAllEmpleados = await pool.query(
-            "SELECT id_empleado, password, nombre, tipo FROM empleado"
-        );
-        //res.json(getAllEmpleados.rows);
-        jwt.verify(req.token, process.env.SECRET_KEY, (error, authData) => {
+        jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
             //console.log("token", req.token, "token")
             if (error) {
                 console.log("error", error)
                 res.sendStatus(401);
             } else {
+                const getAllEmpleados = await pool.query(
+                    "SELECT id_empleado, password, nombre, tipo FROM empleado"
+                );
+                //res.json(getAllEmpleados.rows);
                 res.status(200).send({ code: 1, result: getAllEmpleados.rows });
             }
         });
@@ -68,18 +68,18 @@ router.get("/getAllEmpleados", verifyToken, async (req, res) => {
 
 router.post("/getEmpleado", verifyToken, async (req, res) => {
     try {
-        const { id_empleado } = req.body;
-        const getEmpleado = await pool.query(
-            "SELECT id_empleado, password, nombre, tipo FROM empleado WHERE id_empleado = $1",
-            [id_empleado]
-        );
-        //res.json(getEmpleado.rows[0]);
-        jwt.verify(req.token, process.env.SECRET_KEY, (error, authData) => {
+        jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
             //console.log("token", req.token, "token")
             if (error) {
                 console.log("error", error)
                 res.sendStatus(401);
             } else {
+                const { id_empleado } = req.body;
+                const getEmpleado = await pool.query(
+                    "SELECT id_empleado, password, nombre, tipo FROM empleado WHERE id_empleado = $1",
+                    [id_empleado]
+                );
+                //res.json(getEmpleado.rows[0]);
                 res.status(200).send({ code: 1, result: getEmpleado.rows[0] });
             }
         });
@@ -93,21 +93,21 @@ router.post("/getEmpleado", verifyToken, async (req, res) => {
 
 router.post("/searchEmpleado", verifyToken, async (req, res) => {
     try {
-        const { busqueda } = req.body;
-        const response = await pool.query(
-            `
-            SELECT id_empleado, password, nombre FROM empleado
-            where nombre ilike  '%'|| $1 || '%'
-            `,
-            [busqueda]
-        )
-        //res.json(response.rows);
-        jwt.verify(req.token, process.env.SECRET_KEY, (error, authData) => {
+        jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
             //console.log("token", req.token, "token")
             if (error) {
                 console.log("error", error)
                 res.sendStatus(401);
             } else {
+                const { busqueda } = req.body;
+                const response = await pool.query(
+                    `
+                    SELECT id_empleado, password, nombre FROM empleado
+                    where nombre ilike  '%'|| $1 || '%'
+                    `,
+                    [busqueda]
+                )
+                //res.json(response.rows);
                 res.status(200).send({ code: 1, result: response.rows });
             }
         });
@@ -121,23 +121,22 @@ router.post("/searchEmpleado", verifyToken, async (req, res) => {
 
 router.put("/updateEmpleado", verifyToken, async (req, res) => {
     try {
-        const { id_empleado } = req.body;
-        const { password, nombre } = req.body;
-        const updateEmpleado = await pool.query(
-            "UPDATE empleado SET password = $1, nombre = $2 WHERE id_empleado = $3",
-            [password, nombre, id_empleado]
-        );
-        const getEmpleado = await pool.query(
-            "SELECT id_empleado, password, nombre FROM empleado WHERE id_empleado = $1",
-            [id_empleado]
-        );
-        //res.json(getEmpleado.rows[0]);
-        jwt.verify(req.token, process.env.SECRET_KEY, (error, authData) => {
+        jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
             //console.log("token", req.token, "token")
             if (error) {
                 console.log("error", error)
                 res.sendStatus(401);
             } else {
+                const { id_empleado, password, nombre, email, tipo } = req.body;
+                const updateEmpleado = await pool.query(
+                    "UPDATE empleado SET password = $1, nombre = $2, email = $3, tipo = $4 WHERE id_empleado = $5",
+                    [password, nombre, email, tipo, id_empleado]
+                );
+                const getEmpleado = await pool.query(
+                    "SELECT id_empleado, password, nombre FROM empleado WHERE id_empleado = $1",
+                    [id_empleado]
+                );
+                //res.json(getEmpleado.rows[0]);
                 res.status(200).send({ code: 1, result: getEmpleado.rows[0] });
             }
         });
@@ -150,18 +149,18 @@ router.put("/updateEmpleado", verifyToken, async (req, res) => {
 
 router.delete("/deleteEmpleado", verifyToken, async (req, res) => {
     try {
-        const { id_empleado } = req.body;
-        const deleteEmpleado = await pool.query(
-            "DELETE FROM empleado WHERE id_empleado = $1",
-            [id_empleado]
-        );
-        //res.json("Empleado was deleted!");
-        jwt.verify(req.token, process.env.SECRET_KEY, (error, authData) => {
+        jwt.verify(req.token, process.env.SECRET_KEY, async (error, authData) => {
             //console.log("token", req.token, "token")
             if (error) {
                 console.log("error", error)
                 res.sendStatus(401);
             } else {
+                const { id_empleado } = req.body;
+                const deleteEmpleado = await pool.query(
+                    "DELETE FROM empleado WHERE id_empleado = $1",
+                    [id_empleado]
+                );
+                //res.json("Empleado was deleted!");
                 res.status(200).send({ code: 1, result: "Empleado was deleted!" });
             }
         });
